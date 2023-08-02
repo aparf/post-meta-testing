451b64bf22cc2add2ce9f8edac4de79a9f01b70b
sasha <56977617+aparf@users.noreply.github.com>
2023-08-02 21:54:37 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,116 @@
+// Get the center and left elements
+const centerEl = document.getElementById("center");
+const leftEl = document.getElementById("left");
+
+
+// Function to calculate the width
+function calculateVerticalWidth() {
+  const desktopWidth = document.getElementById("afterWindow").getBoundingClientRect().width;
+  const mobileWidth = document.getElementById("mobileVersion").getBoundingClientRect().width;
+  let constWidth = 0
+  if (mobileWidth == 0){
+    centerWidth = desktopWidth
+  } else {
+    centerWidth = mobileWidth
+  }
+
+  let width = ((window.innerWidth - centerWidth) / 2);
+  document.getElementById("left").style.width = width + "px";
+  document.getElementById("right").style.width = width + "px";
+}
+
+// Call the function initially 
+calculateVerticalWidth();
+
+// Add resize listener to recalculate on resize
+window.addEventListener("resize", calculateVerticalWidth);
+
+
+
+function dragElement(elmnt) {
+  function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  elmnt.onmousedown = dragMouseDown;
+}
+
+// Make the DIV element draggable:
+dragElement(document.getElementById("mydiv"));
+
+
+window.onload = function() {
+  // find the element that you want to drag.
+  var box = document.getElementById('mydiv');
+  
+  /* listen to the touchMove event,
+  every time it fires, grab the location
+  of touch and assign it to box */
+  
+  box.addEventListener('touchmove', function(e) {
+    // grab the location of touch
+    var touchLocation = e.targetTouches[0];
+    
+    // assign box new coordinates based on the touch.
+    box.style.left = touchLocation.pageX + 'px';
+    box.style.top = touchLocation.pageY + 'px';
+  })
+  
+  /* record the position of the touch
+  when released using touchend event.
+  This will be the drop position. */
+  
+  box.addEventListener('touchend', function(e) {
+    // current box position.
+    var x = parseInt(box.style.left);
+    var y = parseInt(box.style.top);
+  })
+  
+}
+
+
+document.addEventListener("keydown", function(event) {
+  event.preventDefault()
+
+  if ((event.metaKey && event.keyCode == 186) || (event.metaKey && event.shiftKey && event.keyCode == 187)) {
+    let scale = parseFloat(window.getComputedStyle(document.getElementById("mydiv")).transform.match(/^matrix\(([^,]*)/)[1]); 
+    scale += 0.1;
+
+    document.getElementById("mydiv").style.transform = "scale(" + scale + ")";
+  } 
+
+  if (event.metaKey && event.keyCode == 189) {
+    let scale = parseFloat(window.getComputedStyle(document.getElementById("mydiv")).transform.match(/^matrix\(([^,]*)/)[1]); 
+    scale -= 0.1;
+
+    document.getElementById("mydiv").style.transform = "scale(" + scale + ")";
+  } 
+
+})
