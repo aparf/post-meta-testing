c6303c54c7eb2591093fb59413fb40b3ba1c08f3
sasha <56977617+aparf@users.noreply.github.com>
2023-08-08 17:38:01 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 
-
+//check whether mobile browser is used 
+if (window.navigator.maxTouchPoints > 0) {
+  alert("MOBILE DEVICE")
+        var meta = document.createElement('meta');
+        meta.name = "viewport";
+        meta.content = "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no";
+        document.getElementsByTagName('head')[0].appendChild(meta);
+      }
 
 // Get the center and left elements
 const centerEl = document.getElementById("center");
 const leftEl = document.getElementById("left");
 
-// const rootElement = document.getElementById("mobileVersion")
-// const viewPortH = rootElement.getBoundingClientRect().height;
-// const windowH = window.innerHeight;
 
-// const browserUiBarsH = viewPortH - windowH;
-// rootElement.style.height = `calc(100vh - ${browserUiBarsH}px)`;
-// document.getElementById("left").style.height = `calc(100vh - ${browserUiBarsH}px)`;
-// document.getElementById("right").style.height = `calc(100vh - ${browserUiBarsH}px)`;
-
+// adjust the height based
 const rootElement = document.getElementById("container");
 const viewPortH = rootElement.getBoundingClientRect().height;
 const windowH = window.innerHeight;
@@ -78,83 +78,169 @@ function dragElement(elmnt) {
   function closeDragElement() {
     // stop moving when mouse button is released:
     document.onmouseup = null;
-    document.onmousemove = null;
+    document.onmousemove = null; 
   }
 
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   elmnt.onmousedown = dragMouseDown;
 }
-
+1
 // Make the DIV element draggable:
 dragElement(document.getElementById("mydiv"));
 
 
-window.onload = function() {
-  // find the element that you want to drag.
-  var box = document.getElementById('mydiv');
+// window.onload = function() {
+//   // find the element that you want to drag.
+//   var box = document.getElementById('mydiv');
   
-  var delta = { x: 0, y: 0 }; // how much we've moved
+//   var delta = { x: 0, y: 0 }; // how much we've moved
 
-  box.addEventListener('touchstart', function(e) {
-    // record the initial touch point
-    var touch = e.targetTouches[0];
-    delta.x = touch.pageX - box.offsetLeft;
-    delta.y = touch.pageY - box.offsetTop;
-  });
+//   box.addEventListener('touchstart', function(e) {
+//     // record the initial touch point
+//     var touch = e.targetTouches[0];
+//     delta.x = touch.pageX - box.offsetLeft;
+//     delta.y = touch.pageY - box.offsetTop;
+//   });
 
-  box.addEventListener('touchmove', function(e) {
-    // grab the location of touch
-    var touchLocation = e.targetTouches[0];
-    
-    // assign box new coordinates based on the touch.
-    box.style.left = touchLocation.pageX - delta.x + 'px';
-    box.style.top = touchLocation.pageY - delta.y + 'px';
+//   box.addEventListener('touchmove', function(e) {
+//     if (e.targetTouches.length === 1) { 
+//       // grab the location of touch
+//       var touchLocation = e.targetTouches[0];
+      
+//       // assign box new coordinates based on the touch.
+//       box.style.left = touchLocation.pageX - delta.x + 'px';
+//       box.style.top = touchLocation.pageY - delta.y + 'px';
 
-    // prevent page scrolling
-    e.preventDefault();
-  });
+//       // prevent page scrolling
+//       e.preventDefault();
+//     }
+//   });
 
-  /* record the position of the touch
-  when released using touchend event.
-  This will be the drop position. */
+//   /* record the position of the touch
+//   when released using touchend event.
+//   This will be the drop position. */
   
-  box.addEventListener('touchend', function(e) {
-    // current box position.
-    var x = parseInt(box.style.left);
-    var y = parseInt(box.style.top);
-  });
-}
+//   box.addEventListener('touchend', function(e) {
+//     // current box position.
+//     var x = parseInt(box.style.left);
+//     var y = parseInt(box.style.top);
+//   });
+// }
 
 
-alert("VERSION 11")
+alert("VERSION 23")
 
-// var af = new AlloyFinger(document.getElementById("mydiv"), {
-//   pinchStart: function () {
-//     initScale = document.getElementById("mydiv").scaleX;
-//   },
-//   pinch: function (evt) {
-//       document.getElementById("mydiv").scaleX = document.getElementById("mydiv").scaleY = initScale * evt.zoom;
-//   }})
+
+
 
 
 var mydiv = document.getElementById('mydiv');
+mydiv.style.transformOrigin = 'center center';
 
- new AlloyFinger(mydiv, {
-            multipointStart: function () {
-                // save initial size and position
-                this.initialWidth = mydiv.offsetWidth;
-                this.initialHeight = mydiv.offsetHeight;
-            },
-            pinch: function (evt) {
-                // scale the div
-                var scale = evt.zoom;
-                mydiv.style.width = this.initialWidth * scale + 'px';
-                mydiv.style.height = this.initialHeight * scale + 'px';
-                // adjust position to scale from the center
-                mydiv.style.left = (initialPos.left - (mydiv.offsetWidth - this.initialWidth) / 2) + 'px';
-                mydiv.style.top = (initialPos.top - (mydiv.offsetHeight - this.initialHeight) / 2) + 'px';
-            }
-        });
+var initialPos = { left: mydiv.offsetLeft, top: mydiv.offsetTop };
+console.log(initialPos)
+
+
+
+
+
+var mydiv = document.getElementById('mydiv');
+var initScale = 1; // You should initialize the scale to 1, which stands for 100% (the original size)
+
+
+console.log(mydiv.style.left)
+
+new AlloyFinger(mydiv, {
+    multipointStart: function () {
+        // Fetch the current scale of the div if it's already been scaled
+        // var style = window.getComputedStyle(mydiv);
+
+        initScale = parseFloat(window.getComputedStyle(document.getElementById("mydiv")).transform.match(/^matrix\(([^,]*)/)[1]); 
+
+
+    },
+
+    pinch: function (evt) {
+        // Scale the div
+        // var initScale = parseFloat(window.getComputedStyle(document.getElementById("mydiv")).transform.match(/^matrix\(([^,]*)/)[1]); 
+        var scale = initScale;
+
+        if(evt.zoom >= 1){
+          scale += 0.25
+      
+        } else{
+          scale -= 0.25
+  
+        }
+
+       mydiv.style.transform = 'scale(' + scale + ')';
+
+    },
+
+
+    // pressMove: function(evt){
+    //   console.log(mydiv.style.left)
+    //   if (!(mydiv.style.left)){
+    //     var x = evt.deltaX;
+    //     var y = evt.deltaY;
+    //   } else {
+    //     var x = parseInt(mydiv.style.left) + evt.deltaX;
+    //     var y = parseInt(mydiv.style.top) + evt.deltaY;
+    //   }
+      
+    //   mydiv.style.left = x + 'px';
+    //   mydiv.style.top = y + 'px';
+    // }
+
+
+    pressMove: function(evt){
+      var rect = mydiv.getBoundingClientRect();
+      
+      // If left and top are not set, use the initial position of the div
+      var x = (mydiv.style.left) ? parseInt(mydiv.style.left) + evt.deltaX : rect.left + evt.deltaX;
+      var y = (mydiv.style.top) ? parseInt(mydiv.style.top) + evt.deltaY : rect.top + evt.deltaY;
+      
+      mydiv.style.left = x + 'px';
+      mydiv.style.top = y + 'px';
+      mydiv.style.position = 'absolute';  // you also need to set the position to absolute to make left and top work
+    }
+
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+// new AlloyFinger(mydiv, {
+//     multipointStart: function () {
+//         // save initial size and position
+//         this.initialWidth = mydiv.offsetWidth;
+//         this.initialHeight = mydiv.offsetHeight;
+//     },
+
+//     pinch: function (evt) {
+
+//         // scale the div
+//         var scale = evt.zoom;
+
+//         // let scale = parseFloat(window.getComputedStyle(document.getElementById("mydiv")).transform.match(/^matrix\(([^,]*)/)[1]); 
+//         // scale += 0.1;
+//         mydiv.style.width = this.initialWidth * scale  + 'px';
+//         mydiv.style.height = this.initialHeight * scale + 'px';
+//         // adjust position to scale from the center
+//         mydiv.style.left = (initialPos.left - (mydiv.offsetWidth - this.initialWidth) / 2) + 'px';
+//         mydiv.style.top = (initialPos.top - (mydiv.offsetHeight - this.initialHeight) / 2) + 'px';
+//     }
+// });
+
 
 document.addEventListener("keydown", function(event) {
   event.preventDefault()
@@ -173,4 +259,11 @@ document.addEventListener("keydown", function(event) {
     document.getElementById("mydiv").style.transform = "scale(" + scale + ")";
   } 
 
+})
+
+
+var button = document.getElementById("button"); 
+
+button.addEventListener("click", () => {
+  alert("CLICK")
 })
